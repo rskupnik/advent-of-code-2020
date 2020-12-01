ef12a7031ee796f51b9f2f473e508f26131809ab
Radosław Skupnik <rskupnik@gmail.com>
2020-12-01 19:56:04 +0100

    Fix comment

--- 01/src/main/java/com/github/rskupnik/adventofcode/Basic.java
+++ 01/src/main/java/com/github/rskupnik/adventofcode/Basic.java
@@ -18,7 +18,7 @@ public class Basic {
         // Put in a TreeSet to sort the numbers for easy lookup
         Set<Integer> inputSet = input.lines().map(Integer::valueOf).collect(Collectors.toCollection(TreeSet::new));
 
-        // For each number, subtract it from 2020 and look for a match in the set. Start from the end because it's more probable to find it there.
+        // For each number, subtract it from 2020 and look for a match in the set
         for (int num : inputSet) {
             inputSet.stream().parallel().filter(i -> i == 2020 - num).findFirst().ifPresent(i -> {
                 System.out.println("Answer is: " + (i * num));
